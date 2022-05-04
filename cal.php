b28302662eb0dd53a5a346ebbecc3f8e7b458351
unknown <aahashish100@gmail.com>
2022-05-04 11:47:26 +0545

    Meter reading

---
+++ cal.php
@@ -0,0 +1,26 @@
+
+<?php
+
+$digit = $_POST["$digit"];
+$result = calculate_bill($digit);
+
+function calculate_bill($digit){
+    $units_first = 3.50;
+    $units_second = 4.00;
+    $units_third = 5.20;
+    $units_fourth = 6.50;
+switch ($current_unit-$previous_unit) {
+    case '':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+}
+
+$numbers = $_POST["digit"];
+echo "Your number is " . $numbers;
+
+?>
